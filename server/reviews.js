78295f885b545248cecfdf5cb09111967556afe5
ScoutOK <ohkeefe@gmail.com>
2016-11-01 15:57:18 -0400

    added review routing file with single route

---
+++ server/reviews.js
@@ -0,0 +1,15 @@
+import db = from 'APP/db';
+import express from 'express';
+import Review from 'APP/db/models/product'
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  Review.findAll()
+  .then(reviews =>{
+    res.json(reviews)
+  })
+  .catch(next)
+})
+
+
