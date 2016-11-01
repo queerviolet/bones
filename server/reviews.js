639c7901b2659b6da45807c9a4c7d3016b5be279
ScoutOK <ohkeefe@gmail.com>
2016-11-01 16:58:55 -0400

    filled in rest of requested routes

--- server/reviews.js
+++ server/reviews.js
@@ -7,10 +7,50 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   Review.findAll()
   .then(reviews =>{
-    res.json(reviews)
+    res.json(reviews);
   })
   .catch(next)
 })
+
+router.get('/:productId', (req, res, next) => {
+  Review.findAll({where: {product_id: req.params.productId}})
+  .then(reviews => {
+    res.json(reviews);
+  })
+  .catch(next)
+})
+
+router.get('/users/:userId', (req, res, next) => {
+  Review.findAll({where: {user_id: req.params.userId}})
+  .then(reviews => {
+    res.json(reviews);
+  })
+  .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+  Review.create(req.body)
+  .then( review => {
+    res.status(200).send('review created')
+  })
+  .catch(next)
+})
+
+router.put('/:reviewId', (req, res, next) => {
+  Review.update({req.body}, {where: {id: req.params.reviewId}})
+  .then( review => {
+    res.status(204).send('review updated')
+  })
+})
+
+router.delete('/:reviewId', (req, res, next) => {
+  Review.destroy({where: {id: req.params.reviewId}})
+  .then(() => {
+    res.send();
+  })
+})
+
+
 
 
 module.exports = router
