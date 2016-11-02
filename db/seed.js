9a4201e4d47580add32f1444e374f6978bea951e
Kat Guthrie <kathrynguthrie@gmail.com>
2016-11-02 17:32:36 -0400

    updated the seed file

--- db/seed.js
+++ db/seed.js
@@ -1,13 +1,37 @@
 const db = require('APP/db')
 
 const seedUsers = () => db.Promise.map([
-  {name: 'so many', email: 'god@example.com', password: '1234'},
-  {name: 'Barack Obama', email: 'barack@example.gov', password: '1234'},
+  {firstName: 'Michelle', lastName: 'Obama', email: 'michelle@firstlady.rocks', password: '1234', username: 'totalbadass'},
+  {firstName: 'Barack', lastName: 'Obama', email: 'barack@president.rocks', password: '1234', username: 'realpowerfuldude'},
 ], user => db.model('users').create(user))
+
+const seedProducts = () => db.Promise.map([
+  {name: `Hillary's Pantsuit`, price: 350.00, categories: ['Clothing']},
+  {name: `Elijah Woods's Frodo Ring`, price: 13000, categories:['Film', 'Fantasy']},
+  {name: `Emma Watson's Hermione Wig`, price: 175.00,categories: ['Costume', 'Hair']},
+], product => db.model('products').create(product))
+
+const seedCelebs = () => db.Promise.map([
+  {name: 'Hillary Clinton', celebType: 'Future President',list: 'A', alive: true},
+  {name: 'Elijah Wood', celebType: 'actor', list: 'A', alive: true},
+  {name: 'Emma Watson', celebType: 'actor', list: 'A', alive: true},
+], celeb => db.model('celebs').create(celeb))
+
+const seedReviews = () => db.Promise.map([
+  {stars: 2, text: 'My dog does not enjoy wearing these dog collars even though he loooooves Angelina Jolie'},
+  {stars: 5, text: 'OMG Frodo wore this!! My ring finger has never been happier'},
+  {stars: 4, text: 'This ring is pretty great, but Elijah Wood didn\'t really care when I showed him that I bought it and that offends me. 4 stars. I\'ll show you, Elijah!'},
+], review => db.model('reviews').create(review))
 
 db.didSync
   .then(() => db.sync({force: true}))
   .then(seedUsers)
   .then(users => console.log(`Seeded ${users.length} users OK`))
+  .then(seedProducts)
+  .then(products => console.log(`Seeded ${products.length} products OK`))
+  .then(seedCelebs)
+  .then(celebs => console.log(`Seeded ${celebs.length} celebs OK`))
+  .then(seedReviews)
+  .then(reviews => console.log(`Seeded ${reviews.length} reviews OK`))
   .catch(error => console.error(error))    
   .finally(() => db.close())
