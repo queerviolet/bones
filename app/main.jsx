c48f88f3b26e984f656b302196991358ade89138
ScoutOK <ohkeefe@gmail.com>
2016-11-02 12:16:40 -0400

    added navbar to main.jsx

--- app/main.jsx
+++ app/main.jsx
@@ -5,9 +5,11 @@ import { Provider } from 'react-redux'
 
 import store from './store'
 import Root from './components/Root'
+import NavBar from './components/NavBar'
 
 render (
   <Provider store={store}>
+    <NavBar />
     <Root/>
   </Provider>,
   document.getElementById('main')
