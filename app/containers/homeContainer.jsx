a9787a985cfde083130d84ba0a009005363ee9eb
Kenneth Moy <moykenneth91@gmail.com>
2016-11-02 21:05:41 -0400

    added jQuery to index.html added a Home Container and Component This closes #26

---
+++ app/containers/homeContainer.jsx
@@ -0,0 +1,9 @@
+'use strict'
+
+import { connect } from 'react-redux';
+import { homeContainer } from '../components/homeComponent'
+
+const mapStateToProps = (state, ownProps) => { }
+const mapDispatchToProps = (dispatch, ownProps) => { }
+
+export default connect(() => ({}), {})(homeComponent);
--- app/main.jsx
+++ app/main.jsx
@@ -7,11 +7,12 @@ import { Provider } from 'react-redux'
 import store from './store'
 import Root from './components/Root'
 import Login from './components/Login'
+import homeComponent from './components/homeComponent'
 
 render (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={Root} />
+      <Route path="/" component={homeComponent} />
       <Route path="/login" component={Login} />      
     </Router>
   </Provider>,
