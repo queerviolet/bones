76dd68a97c36a63e445bd1eced0368998bfd040e
Elaine <ljc391@nyu.edu>
2016-11-03 16:06:08 -0400

    user component

--- db/seed.js
+++ db/seed.js
@@ -1,13 +1,13 @@
 const db = require('APP/db')
 
 const seedUsers = () => db.Promise.map([
-  {name: 'so many', email: 'god@example.com', password: '1234'},
-  {name: 'Barack Obama', email: 'barack@example.gov', password: '1234'},
+  {firstName: 'so many', lastName:'lastName', email: 'god@example.com', password: '1234'},
+  {firstName: 'Barack Obama',lastName:'lastName', email: 'barack@example.gov', password: '1234'},
 ], user => db.model('users').create(user))
 
 db.didSync
   .then(() => db.sync({force: true}))
   .then(seedUsers)
   .then(users => console.log(`Seeded ${users.length} users OK`))
-  .catch(error => console.error(error))    
+  .catch(error => console.error(error))
   .finally(() => db.close())
