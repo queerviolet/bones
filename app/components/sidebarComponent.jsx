eea2e872cc2a631cb6949ffa8b0f0c1104a1a22e
Kenneth Moy <moykenneth91@gmail.com>
2016-11-03 03:04:40 -0400

    linked css to index.html added some basic jsx for a sidebar rendering info

--- app/components/sidebarComponent.jsx
+++ app/components/sidebarComponent.jsx
@@ -5,7 +5,26 @@ import React from 'react'
 export default () => {
     return (
         <div className="sidebar-container">
-            Hello!
+            <div id="wrapper">
+                <div id="sidebar-wrapper">
+                    <ul className="sidebar-nav">
+                        <li className="sidebar-brand"><a href="#">Home</a></li>
+                        <li><a href="#">Another link</a></li>
+                        <li><a href="#">Next link</a></li>
+                        <li><a href="#">Last link</a></li>
+                    </ul>
+                </div>
+                <div id="page-content-wrapper">
+                    <div className="page-content">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-12">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
