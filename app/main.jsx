7f8d504f896d3756589d9ec3f4a492a9d7169464
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-11-03 19:30:37 -0400

    changed colors on home page, added cart icon

--- app/main.jsx
+++ app/main.jsx
@@ -1,6 +1,6 @@
 'use strict'
 import React from 'react'
-import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
+import {Router, Route, IndexRedirect, browserHistory, IndexRoute} from 'react-router'
 import {render} from 'react-dom'
 import { Provider } from 'react-redux'
 
@@ -10,14 +10,16 @@ import Root from './components/Root'
 import Login from './components/Login'
 import homeContainer from './containers/homeContainer'
 import productContainer from './containers/productContainer'
+import allProductsContainer from './containers/allproductsContainer'
 
 render (
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={homeContainer}>
+        <IndexRoute component={allProductsContainer} />
        <Route path="products/:id" component={productContainer} />
       </Route>
-      <Route path="/login" component={Login} />      
+      <Route path="/login" component={Login} />
     </Router>
   </Provider>,
   document.getElementById('main')
