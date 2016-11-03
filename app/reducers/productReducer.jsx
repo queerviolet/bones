33cfc54b5d048e58e0cc2f82cbab2608fb04dc6d
Kenneth Moy <moykenneth91@gmail.com>
2016-11-03 17:58:56 -0400

    fixed syntax in productReducer

--- app/reducers/productReducer.jsx
+++ app/reducers/productReducer.jsx
@@ -2,8 +2,8 @@
 
 import { RECEIVE_ALL_PRODUCTS } from '../actions/productActions';
 
-// Reducer for Categories
-export default function categoriesReducer(prevState = [], action){
+// Reducer for Products
+export default function productsReducer(prevState = [], action){
     switch(action.type){
         case RECEIVE_ALL_PRODUCTS: return action.products;
         default: return prevState;
