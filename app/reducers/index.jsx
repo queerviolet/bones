138897f79d36427290bb084d741fb6e97ea49aac
Kenneth Moy <moykenneth91@gmail.com>
2016-11-04 11:55:33 -0400

    fixed a leftover bug from the merge

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -3,7 +3,7 @@ import cartReducer from './cartReducer';
 
 const rootReducer = combineReducers({
   auth: require('./auth').default,
-  products: require('./productReducer').default,
+  products: require('./productsReducer').default,
   categories: require('./categoryReducer').default,
   cart: cartReducer
 });
