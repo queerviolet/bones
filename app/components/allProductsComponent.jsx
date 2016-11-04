4a48d3f06a2c3dc4e43c7a86973713ba4722b6aa
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-11-04 19:59:34 -0400

    fixed more info btn

--- app/components/allProductsComponent.jsx
+++ app/components/allProductsComponent.jsx
@@ -5,28 +5,28 @@ import { Link } from 'react-router';
 
 export default ({products, addToCart, cart}) => (
 
-        <div className="row text-center">
-          {
-            // checkoutLocalStorage(cart);
-            products && products.map((product, index) => {
-              return (
-                <div key={`${index}`} className="col-md-3 col-sm-6 product-boxes">
-                    <div className="thumbnail">
-                        <img src={product.photoUrl} alt=""/>
-                        <div className="caption">
-                            <h3>{product.title}</h3>
-                            <p>{product.description}</p>
-                            <p>${product.price}</p>
-                            <p>
-                                <a onClick={() => {addToCart(product.id, 1);}} href="#" className="btn btn-primary">Add To Cart!</a>
-                                <Link to={"/products/" + product.id} className="btn btn-default">More Info</Link>
-                            </p>
-                        </div>
+    <div className="row text-center">
+      {
+        // checkoutLocalStorage(cart);
+        products && products.map((product, index) => {
+          return (
+            <div key={`${index}`} className="col-md-3 col-sm-6 product-boxes">
+                <div className="thumbnail">
+                    <img src={product.photoUrl} alt=""/>
+                    <div className="caption">
+                        <h3>{product.title}</h3>
+                        <p>{product.description}</p>
+                        <p>${product.price}</p>
+                        <p>
+                            <a onClick={() => {addToCart(product.id, 1);}} href="#" className="btn btn-primary">Add To Cart!</a>
+                            <Link to={"/products/" + product.id} className="btn btn-default">More Info</Link>
+                        </p>
                     </div>
                 </div>
-              )
-            })
-          }
-        </div>
+            </div>
+          )
+        })
+      }
+    </div>
 );
 
