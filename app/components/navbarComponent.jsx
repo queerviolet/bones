0931c748c88e18c4b2f62257ef23a2374a08c09b
Kenneth Moy <moykenneth91@gmail.com>
2016-11-05 12:50:29 -0400

    added action creators for search modified navbar to submit searches

--- app/components/navbarComponent.jsx
+++ app/components/navbarComponent.jsx
@@ -10,25 +10,23 @@ Navbar has:
 */
 
 export default () => (
-
-  <nav className="navbar navbar-inverse navbar-fixed-top">
-    <a id="cart" className="btn" data-placement="bottom" title="Ready to Buy?" >
-      <span className="glyphicon glyphicon-shopping-cart"></span>
-    </a>
-    <div className="container">
-      <div className="navbar-header">
-        <Link to={"/"}className="navbar-brand">CodeCommerce</Link>
-          <div className="form-group">
-        <input className="navbar-form navbar-left" role="search"
-              type="text" className="form-control" 
-              type="search"
-              placeholder="BST, loops, etc..."
-              onChange={this.searchKey.bind(this)}>
-          <button type="submit" className="btn btn-default" onSubmit={() => searchKey(this.props.value)}></button>
-        </input>
+<nav className="navbar navbar-inverse navbar-fixed-top">
+  <Link to="cart" id="cart" className="btn" data-placement="bottom" title="Ready to Buy?" >
+    <span className="glyphicon glyphicon-shopping-cart"></span>
+  </Link>
+  <div className="container">
+    <div className="navbar-header">
+      <Link to={"/"}className="navbar-brand">CodeCommerce</Link>
+      <Link to={"/login"}className="nav navbar navbar-right">Login</Link>
+      <form className="navbar-form navbar-left" role="search">
+        <div className="form-group">
+          <input type="text" className="form-control" 
+            placeholder="BST, loops, etc..." onSubmit={event => this.props.onSubmitName(event.target.value)}></input>
         </div>
-      </div>
+        <button type="submit" className="btn btn-default">Search</button>
+      </form>
     </div>
-  </nav>
+  </div>
+</nav>
 );
 
--- app/containers/navbarContainer.js
+++ app/containers/navbarContainer.js
@@ -2,12 +2,19 @@
 
 import { connect } from 'react-redux';
 import navbarComponent from '../components/navbarComponent';
+import { receiveNamedProductsFromServer } from '../actions/productsActions';
+import { browserHistory } from 'react-router'
 
-const mapStateToProps = (state, ownProps) => ({
-    categories: state.categories
-})
+const mapStateToProps = (state, ownProps) => ({})
+
+const callback = url => browserHistory.push(`${url}`)
 const mapDispatchToProps = (dispatch, ownProps) => {
-    return {};
+    return {
+        onSubmitName: (name) => {
+            console.log('submitted input', name);
+            dispatch(receiveNamedProductsFromServer(name, callback))
+        }
+    };
  }
 
 export default connect(mapStateToProps, mapDispatchToProps)(navbarComponent);
