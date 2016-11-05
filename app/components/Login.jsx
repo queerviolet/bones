110b1780f1c7ad15dd5ab955d64f6541fe965b01
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-11-04 21:00:23 -0400

    added login page, css styles, and comments

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
 export const Login = ({ login }) => (
-  <form onSubmit={evt => {
-    evt.preventDefault()
-    login(evt.target.username.value, evt.target.password.value)
-  } }>
-    <input name="username" />
-    <input name="password" type="password" />
-    <input type="submit" value="Login" />
-  </form>
+  <div className="container">
+    <div className="wrapper">
+      <form className="form-signin" onSubmit={evt => {
+        evt.preventDefault()
+        login(evt.target.username.value, evt.target.password.value)
+      } }>
+        <h3 className="form-signin-heading">Welcome! Please Sign In</h3>
+        <hr className="colorgraph"></hr>
+        <input type="text" className="form-control" name="username" placeholder="Username"/>
+        <input name="password" className="form-control" type="password" placeholder="Password"/>
+        <button className="btn btn-lg btn-primary btn-block" type="Submit" value="Login">Login</button>
+      </form>
+    </div>
+  </div>
 )
 
 import {login} from 'APP/app/reducers/auth'
