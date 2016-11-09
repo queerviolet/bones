6f55ae3ad879bb4691e65568ab284a812546cc4e
Jisoo <rjisoo@users.noreply.github.com>
2016-11-08 19:49:18 -0500

    fixed all products align (#122)
    Design: fix all product align #113

--- browser/components/products/AllProducts.js
+++ browser/components/products/AllProducts.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { AutoComplete, SelectField, MenuItem } from 'material-ui'
-import { formatPrice, getAvgRating, getStars, categories } from '../../utils'
+import { formatPrice, getAvgRating, getStars } from '../../utils'
 
 export default ({ products, category, handleChange }) => {
   return (
@@ -23,38 +23,45 @@ export default ({ products, category, handleChange }) => {
             onChange={(event, key, value) => handleChange("category", value) }
           >
           {
-            Object.keys(categories).map((key, i) => {
-              return <MenuItem key={i} value={key} primaryText={categories[key]} />
+            categories.map((category, i) => {
+              return <MenuItem key={i} value={category.key} primaryText={category.display} />
             })
           }
           </SelectField>
         </div>
       </div>
-      <div className="row product-results">
       {
-        products.map((product) => {
-          const avgRating = getAvgRating(product.reviews)
-          return (
-          <div key={ product.id } className="col-sm-4 col-lg-4 col-md-4">
-            <div className="thumbnail">
-              <img src={product.images[0]} alt="" />
-              <div className="caption">
-                <h4 className="pull-right">{ formatPrice(product.price) }</h4>
-                <h4>
-                  <Link to={`/products/${product.id}`}>{product.name}</Link>
-                </h4>
-              </div>
-              <div className="pull-right ratings">
+        products.map((product,rowIndex) => {
+          return rowIndex %3 === 0 ? (
+            <div className="row product-results">
               {
-                getStars(avgRating)
+                products.filter((product, colIndex) => [rowIndex, rowIndex+1, rowIndex+2].some(i => colIndex === i))
+                .map((product) => {
+                  const avgRating = getAvgRating(product.reviews)
+                  return (
+                  <div key={ product.id } className="col-sm-4 col-lg-4 col-md-4">
+                    <div className="thumbnail">
+                      <img src={product.images[0]} alt="" className="img-sm-4 img-lg-4 img-md-4" />
+                      <div className="caption">
+                        <h4 className="pull-right">{ formatPrice(product.price) }</h4>
+                        <h4>
+                          <Link to={`/products/${product.id}`}>{product.name}</Link>
+                        </h4>
+                      </div>
+                      <div className="pull-right ratings">
+                      {
+                        getStars(avgRating)
+                      }
+                      </div>
+                    </div>
+                  </div>
+                  )
+                })
               }
-              </div>
             </div>
-          </div>
-          )
+            ) : null
         })
       }
-      </div>
     </div>
   )
 };
