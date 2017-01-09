307a644ca5e41cd536f03fb778d1e91f41de7dfe
Benjamin Saphier <b.saphier@gmail.com>
2017-01-09 11:52:00 -0500

    default commit message

--- server/auth.js
+++ server/auth.js
@@ -1,34 +1,34 @@
-const app = require('APP'), {env} = app
-const debug = require('debug')(`${app.name}:auth`)
-const passport = require('passport')
+const app = require('APP'), {env} = app;
+const debug = require('debug')(`${app.name}:auth`);
+const passport = require('passport');
 
-const User = require('APP/db/models/user')
-const OAuth = require('APP/db/models/oauth')
-const auth = require('express').Router()
+const User = require('APP/db/models/user');
+const OAuth = require('APP/db/models/oauth');
+const auth = require('express').Router();
 
 
 /*************************
  * Auth strategies
- * 
+ *
  * The OAuth model knows how to configure Passport middleware.
  * To enable an auth strategy, ensure that the appropriate
  * environment variables are set.
- * 
+ *
  * You can do it on the command line:
- * 
+ *
  *   FACEBOOK_CLIENT_ID=abcd FACEBOOK_CLIENT_SECRET=1234 npm start
- * 
+ *
  * Or, better, you can create a ~/.$your_app_name.env.json file in
  * your home directory, and set them in there:
- * 
+ *
  * {
  *   FACEBOOK_CLIENT_ID: 'abcd',
  *   FACEBOOK_CLIENT_SECRET: '1234',
  * }
- * 
+ *
  * Concentrating your secrets this way will make it less likely that you
  * accidentally push them to Github, for example.
- * 
+ *
  * When you deploy to production, you'll need to set up these environment
  * variables with your hosting provider.
  **/
@@ -44,7 +44,7 @@ OAuth.setupStrategy({
     callbackURL: `${app.rootUrl}/api/auth/login/facebook`,
   },
   passport
-})
+});
 
 // Google needs the GOOGLE_CONSUMER_SECRET AND GOOGLE_CONSUMER_KEY
 // environment variables.
@@ -57,7 +57,7 @@ OAuth.setupStrategy({
     callbackURL: `${app.rootUrl}/api/auth/login/google`,
   },
   passport
-})
+});
 
 // Github needs the GITHUB_CLIENT_ID AND GITHUB_CLIENT_SECRET
 // environment variables.
@@ -70,66 +70,65 @@ OAuth.setupStrategy({
     callbackURL: `${app.rootUrl}/api/auth/login/github`,
   },
   passport
-})
+});
 
 // Other passport configuration:
 
 passport.serializeUser((user, done) => {
-  debug('will serialize user.id=%d', user.id)
-  done(null, user.id)
-  debug('did serialize user.id=%d', user.id)
-})
+  debug('will serialize user.id=%d', user.id);
+  done(null, user.id);
+  debug('did serialize user.id=%d', user.id);
+});
 
 passport.deserializeUser(
   (id, done) => {
-    debug('will deserialize user.id=%d', id)
+    debug('will deserialize user.id=%d', id);
     User.findById(id)
       .then(user => {
-        debug('deserialize did ok user.id=%d', user.id)
-        done(null, user)
+        debug('deserialize did ok user.id=%d', user.id);
+        done(null, user);
       })
       .catch(err => {
-        debug('deserialize did fail err=%s', err)
-        done(err)
-      })
+        debug('deserialize did fail err=%s', err);
+        done(err);
+      });
   }
-)
+);
 
 passport.use(new (require('passport-local').Strategy) (
   (email, password, done) => {
-    debug('will authenticate user(email: "%s")', email)
+    debug('will authenticate user(email: "%s")', email);
     User.findOne({where: {email}})
       .then(user => {
         if (!user) {
-          debug('authenticate user(email: "%s") did fail: no such user', email)
-          return done(null, false, { message: 'Login incorrect' })
+          debug('authenticate user(email: "%s") did fail: no such user', email);
+          return done(null, false, { message: 'Login incorrect' });
         }
         return user.authenticate(password)
           .then(ok => {
             if (!ok) {
-              debug('authenticate user(email: "%s") did fail: bad password')              
-              return done(null, false, { message: 'Login incorrect' })
+              debug('authenticate user(email: "%s") did fail: bad password');
+              return done(null, false, { message: 'Login incorrect' });
             }
-            debug('authenticate user(email: "%s") did ok: user.id=%d', user.id)
-            done(null, user)              
-          })
+            debug('authenticate user(email: "%s") did ok: user.id=%d', user.id);
+            done(null, user);
+          });
       })
-      .catch(done)
+      .catch(done);
   }
-))
+));
 
-auth.get('/whoami', (req, res) => res.send(req.user))
+auth.get('/whoami', (req, res) => res.send(req.user));
 
 auth.post('/:strategy/login', (req, res, next) =>
   passport.authenticate(req.params.strategy, {
     successRedirect: '/'
   })(req, res, next)
-)
+);
 
 auth.post('/logout', (req, res, next) => {
-  req.logout()
-  res.redirect('/api/auth/whoami')
-})
+  req.logout();
+  res.redirect('/api/auth/whoami');
+});
 
-module.exports = auth
-
+module.exports = auth;
