5c5517fdba7022334a85c5375df2a62ed9e09243
Benjamin Saphier <b.saphier@gmail.com>
2017-01-10 15:42:35 -0500

    get all rocks with associated values

---
+++ server/rocks.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const db = require('APP/db');
+const Tag = db.model('tags');
+const Rock = db.model('rocks');
+const Review = db.model('reviews');
+const Category = db.model('categories');
+
+
+const router = require('express').Router();
+
+// get all the rocks and the associated tags, reviews & categories
+router.get('/', (req, res, next) => {
+  Rock.findAll({ include: [Tag, Review, Category] })
+    .then(rocks => res.json(rocks))
+    .catch(next);
+});
+
+module.exports = router;
