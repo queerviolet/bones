1329d305145d812d0b7fe0f025a194ec30e32368
Benjamin Saphier <b.saphier@gmail.com>
2017-01-10 15:22:41 -0500

    default commit message

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-'use strict'
+'use strict';
 
-const {resolve} = require('path')
-const chalk = require('chalk')
-const pkg = require('./package.json')
-const debug = require('debug')(`${pkg.name}:boot`)
+const {resolve} = require('path');
+const chalk = require('chalk');
+const pkg = require('./package.json');
+const debug = require('debug')(`${pkg.name}:boot`);
 
 const nameError =
 `*******************************************************************
@@ -17,40 +17,40 @@ isn't valid. If you don't change it, things won't work right.
 
 Please change it in ${__dirname}/package.json
   ~ xoxo, bones
-********************************************************************`
+********************************************************************`;
 
-const reasonableName = /^[a-z0-9\-_]+$/
+const reasonableName = /^[a-z0-9\-_]+$/;
 if (!reasonableName.test(pkg.name)) {
-  console.error(chalk.red(nameError))
+  console.error(chalk.red(nameError));
 }
 
 // This will load a secrets file from
 //
-//      ~/.your_app_name.env.js 
+//      ~/.your_app_name.env.js
 //   or ~/.your_app_name.env.json
 //
 // and add it to the environment.
 const env = Object.create(process.env)
-  , secretsFile = resolve(env.HOME, `.${pkg.name}.env`)
+  , secretsFile = resolve(env.HOME, `.${pkg.name}.env`);
 try {
-  Object.assign(env, require(secretsFile))
+  Object.assign(env, require(secretsFile));
 } catch (error) {
-  debug('%s: %s', secretsFile, error.message)
-  debug('%s: env file not found or invalid, moving on', secretsFile)  
+  debug('%s: %s', secretsFile, error.message);
+  debug('%s: env file not found or invalid, moving on', secretsFile);
 }
 
 module.exports = {
-  get name() { return pkg.name },
-  get isTesting() { return !!global.it },
+  get name() { return pkg.name; },
+  get isTesting() { return !!global.it; },
   get isProduction() {
-    return process.env.NODE_ENV === 'production'
+    return process.env.NODE_ENV === 'production';
   },
   get baseUrl() {
     return env.BASE_URL || `http://localhost:${PORT}`
   },
   get port() {
-    return env.PORT || 1337
+    return env.PORT || 1337;
   },
   package: pkg,
   env,
-}
+};
