3d5e7525755ddcc6142b8be5da210fdec03d1d82
jimkim <kimjames@nyu.edu>
2017-01-13 17:34:59 -0500

    logged in and signed up

--- app/components/login/Login.jsx
+++ app/components/login/Login.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const Login = ({ login, logout }) => (
+const Login = ({ login, logout, signup }) => (
   <div>
+    <br></br>
+    <h1>Sign-In</h1>
     <form onSubmit={evt => {
       evt.preventDefault();
       login(evt.target.email.value, evt.target.password.value);
     } }>
-      <input name="email" />
-      <input name="password" type="password" />
+      <input name="email" placeholder="e-mail"/>
+      <input name="password" type="password" placeholder="Password"/>
       <input type="submit" value="Login" />
     </form>
     <button onClick={evt => {
@@ -15,7 +17,21 @@ const Login = ({ login, logout }) => (
       logout();
     }}>
       Logout
-    </button>      
+    </button>
+
+    <hr></hr>
+
+    <h1>Sign-Up</h1>
+    <form onSubmit={evt => {
+        evt.preventDefault();
+        signup(evt.target.firstName.value, evt.target.lastName.value, evt.target.email.value, evt.target.password.value )
+      }}>
+      First Name: <input name="firstName" placeholder="First Name"/><br></br>
+      Last Name: <input name="lastName" placeholder="Last Name"/><br></br>
+      Email: <input name="email" placeholder="e-mail"/><br></br>
+    Password: <input name="password" type="password" placeholder="Password"/><br></br>
+      <input type="submit" value="Sign Up!" />
+    </form>
   </div>
 )
 
--- app/components/login/LoginContainer.jsx
+++ app/components/login/LoginContainer.jsx
@@ -1,6 +1,8 @@
-// import {login} from 'APP/app/reducers/auth';
-import {login, logout} from 'APP/app/reducers/auth';
+import {login, logout, signup} from 'APP/app/reducers/auth';
+import {browserHistory} from 'react-router';
 import {connect} from 'react-redux';
+import store from 'APP/app/store';
+
 import Login from './Login';
 
 const mapStateToProps = ({loggedInUser}) => ({loggedInUser});
@@ -8,11 +10,16 @@ const mapStateToProps = ({loggedInUser}) => ({loggedInUser});
 const mapDispatchToProps = (dispatch) => {
   return {
     login: function(email, password) {
-      dispatch(login(email, password));
+      dispatch(login(email, password))
     },
     logout: function() {
       dispatch(logout());
+    },
+    signup: function(firstName, lastName, email, password) {
+      dispatch(signup(firstName, lastName, email, password));
+      browserHistory.push('/');
     }
+
   };
 };
 
