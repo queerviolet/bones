b94c005bb05fcd3356118c7452b230f1255964cc
Tedman Hsu <tedh526@gmail.com>
2017-01-14 16:27:26 -0500

    cssLogin mostly done

--- app/components/login/Login.jsx
+++ app/components/login/Login.jsx
@@ -1,38 +1,54 @@
 import React from 'react';
 
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import Paper from 'material-ui/Paper';
+import {GridList} from 'material-ui/GridList';
+
+const style = {
+  paper: {
+    height: '100vh',
+    textAlign: 'center'
+  },
+  button: {
+    margin: '5%'
+  }
+};
+
 const Login = ({ login, logout, signup }) => (
   <div>
-    <br></br>
-    <h1>Sign-In</h1>
-    <form onSubmit={evt => {
-      evt.preventDefault();
-      login(evt.target.email.value, evt.target.password.value);
-    } }>
-      <input name="email" placeholder="e-mail"/>
-      <input name="password" type="password" placeholder="Password"/>
-      <input type="submit" value="Login" />
-    </form>
-    <button onClick={evt => {
-      evt.preventDefault();
-      logout();
-    }}>
-      Logout
-    </button>
+    <GridList>
+      <Paper zDepth={1} style={style.paper}>
+        <h2>Existing Customers</h2>
+        <form onSubmit={evt => {
+          evt.preventDefault();
+          login(evt.target.email.value, evt.target.password.value);
+        } }>
+          <TextField name="email" floatingLabelText="e-mail"/><br></br>
+          <TextField name="password" type="password" floatingLabelText="Password"/><br></br>
+          <RaisedButton type="submit" label="Log In" primary={true} style={style.button}/>
+        </form>
+        <RaisedButton label="Temporary Logout" secondary={true} onClick={evt => {
+          evt.preventDefault();
+          logout();
+        }}/>
+      </Paper>
 
-    <hr></hr>
-
-    <h1>Sign-Up</h1>
-    <form onSubmit={evt => {
-        evt.preventDefault();
-        signup(evt.target.firstName.value, evt.target.lastName.value, evt.target.email.value, evt.target.password.value )
-      }}>
-      First Name: <input name="firstName" placeholder="First Name"/><br></br>
-      Last Name: <input name="lastName" placeholder="Last Name"/><br></br>
-      Email: <input name="email" placeholder="e-mail"/><br></br>
-    Password: <input name="password" type="password" placeholder="Password"/><br></br>
-      <input type="submit" value="Sign Up!" />
-    </form>
+      <Paper zDepth={1} style={style.paper}>
+        <h2>New Customer Sign-Up</h2>
+        <form onSubmit={evt => {
+            evt.preventDefault();
+            signup(evt.target.firstName.value, evt.target.lastName.value, evt.target.email.value, evt.target.password.value );
+          }}>
+          <TextField name="firstName" floatingLabelText="First Name"/><br></br>
+          <TextField name="lastName" floatingLabelText="Last Name"/><br></br>
+          <TextField name="email" floatingLabelText="e-mail"/><br></br>
+          <TextField name="password" type="password" floatingLabelText="Password"/><br></br>
+          <RaisedButton type="submit" label="Sign Up" primary={true} style={style.button}/>
+        </form>
+      </Paper>
+    </GridList>
   </div>
-)
+);
 
 export default Login;
