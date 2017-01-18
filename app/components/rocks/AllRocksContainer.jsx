32307429cc09db92fcda30976f2a7dddcb5b88cb
Benjamin Saphier <b.saphier@gmail.com>
2017-01-18 10:59:20 -0500

    snackbars

--- app/components/rocks/AllRocksContainer.jsx
+++ app/components/rocks/AllRocksContainer.jsx
@@ -2,7 +2,11 @@ import { connect } from 'react-redux';
 import AllRocks from './AllRocks';
 
 import { addProductToCart } from '../../reducers/cart';
-import { updateQuantity } from '../../reducers/itemQuantityReducer';
+import {
+  updateQuantity,
+  addedItemToCart,
+  handleSnackbarClose
+} from '../../reducers/itemQuantityReducer';
 
 const mapStateToProps = ({ rocks, auth, itemQuantity }, { location: { pathname } }) => ({
   auth,
@@ -15,7 +19,11 @@ const mapDispatchToProps = dispatch => ({
   addProductToCart: (quantity, userId, rockId) =>
     dispatch(addProductToCart(quantity, userId, rockId)),
   updateQuantity: amount =>
-    dispatch(updateQuantity(amount))
+    dispatch(updateQuantity(amount)),
+  addedItemToCart: () =>
+    dispatch(addedItemToCart()),
+  handleSnackbarClose: () =>
+    dispatch(handleSnackbarClose())
 });
 
 export default connect(
